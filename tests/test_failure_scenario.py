0033da0f8bff22b907ce97074d375241b25ade1f
Tainá Cortez <taina.cortez@sou.inteli.edu.br>
2026-06-05 12:20:49 -0300

    Remove falha controlada do CI

--- tests/test_failure_scenario.py
+++
@@ -1,6 +0,0 @@
-def test_intentional_failure_for_ci_metrics_experiment():
-    """
-    Falha intencional utilizada para gerar uma execucao vermelha
-    durante o experimento de metricas do GitHub Actions.
-    """
-    assert 1 == 2
